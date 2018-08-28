7e04d07c743b55df9100bb18a7453e6449426565
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-27 16:32:59 -1000

    sidebar smoke test, header folder made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//modules
+import { RouterModule } from '@angular/router';
 
+//components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+//pages
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {path: '', component: HomeComponent}
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
