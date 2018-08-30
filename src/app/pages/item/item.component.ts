d78a874c5eed916ffdb98467daa20920e38309ec
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-29 16:10:52 -1000

    user can only edit their own items

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BackendService } from '../../services/backend.service';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   templateUrl: './item.component.html',
@@ -8,19 +9,53 @@ import { BackendService } from '../../services/backend.service';
 })
 
 export class ItemComponent implements OnInit {
+  user: object;
   item: any;
+  editing: boolean = false;
+  correctUser: boolean = false;
+
+  editFormData: {
+    description: string;
+    photo: string;
+    status: string;
+    condition: string;
+    manufacturer_make: string;
+    model_name_number: string;
+    notes_details: string;
+  }
 
   constructor(
     private router: Router,
     private backend: BackendService,
+    private session: SessionService,
     private activatedRouter: ActivatedRoute
-  ) { }
+  ) {
+    this.user = this.session.getSession();
+  }
 
   ngOnInit() {
     let itemId = this.activatedRouter.snapshot.paramMap.get('id');
     return this.backend.getItemById(itemId)
       .then(result => {
+        this.editFormData = result[0];
+        if (result[0].created_by === this.user['user_id']) {
+          this.correctUser = true;
+        }
         return this.item = result[0];
       })
+  }
+
+  toggleEdit() {
+    if (this.editing) {
+      return this.editing = false;
+    } else {
+      return this.editing = true;
+    }
+  }
+
+  submitEdit() {
+    console.log(this.editFormData);
+    console.log('this.item :', this.item);
+    console.log('this.user', this.user)
   }
 }
